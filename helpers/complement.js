518d3b797df7f079ae108445cd9d5fdb38bd9300
Mike Vincent Quito <mike_vincent_quito@dlsu.edu.ph>
2021-01-20 10:15:51 +0800

    Fixed complement

--- helpers/complement.js
+++ helpers/complement.js
@@ -4,10 +4,10 @@ module.exports.twosComplement = function (array){
 	array.forEach(function(element){
   		complementedArray.push(Number(!element));
 	});
-
-	for(let i=0; i < complementedArray.length; i++){
+	for(let i=complementedArray.length; i >= 0; i--){
 		if(complementedArray[i] === 1 && carry === 1)
     		complementedArray[i] = 0;
+        
   		else if(complementedArray[i] ===0 && carry === 1){
     		complementedArray[i] = 1;
     		carry = 0;
