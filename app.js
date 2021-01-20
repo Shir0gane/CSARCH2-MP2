f8874def39cfb35c9798868847391aead2b5216d
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2021-01-20 19:15:02 +0800

    Add handler for GET "/divide/:dividend/:divisor"

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const divisionMiddlewares = require("./middlewares/division-middlewares");
+const division = require("./helpers/division");
 
 const port = process.env.PORT || 3000;
 
 let app = express();
+
+app.get("/divide/:dividend/:divisor", divisionMiddlewares.checkOperands, divisionMiddlewares.convertStringToArray, (req, res) => {
+    try{
+        let result = division.nonRestoringDivision(req.params.dividend, req.params.divisor);
+        res.status(200).send(result);
+    }catch(err){
+        res.status(500).send({message: "Internal Server Error."});
+    }
+});
 
 app.listen(port, () => console.log("Listening at port " + port));
