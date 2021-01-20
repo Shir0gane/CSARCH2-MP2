fd26e30fe3046deb51858cdd3e6e9616c4f402b8
Mike Vincent Quito <mike_vincent_quito@dlsu.edu.ph>
2021-01-20 09:24:44 +0800

    Fixed compile errors on complement function

--- helpers/complement.js
+++ helpers/complement.js
@@ -6,10 +6,10 @@ module.exports.twosComplement = function (array){
 	});
 
 	for(let i=0; i < complementedArray.length; i++){
-		if(element === 1 && carry === 1)
-    		complementedArray[index] = 0;
-  		else if(element ===0 && carry === 1){
-    		complementedArray[index] = 1;
+		if(complementedArray[i] === 1 && carry === 1)
+    		complementedArray[i] = 0;
+  		else if(complementedArray[i] ===0 && carry === 1){
+    		complementedArray[i] = 1;
     		carry = 0;
   		}
 	}
