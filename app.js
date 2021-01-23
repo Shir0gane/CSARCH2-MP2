ada13a795a9b9eda10ec1bc63bc7e17bdf47838c
lexcyman <alexandra.mangune@gmail.com>
2021-01-23 20:05:46 +0800

    added public folder, and updated home.hbs

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs');
 
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
