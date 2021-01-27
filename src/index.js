bd726b17a3f637d80db0ba8080b0272517d1da8e
Joshua Kiel Gaurano <joshua_kiel_gaurano@dlsu.edu.ph>
2021-01-27 09:48:22 +0800

    Modify UI behavior
    Show message in result section if the result has not yet been computed or no input is provided. Step-by-step final result appear only after last step

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ let remainder = document.getElementById("remainder");
 let initRegisterA = document.getElementById("init-register-A");
 let initRegisterQ = document.getElementById("init-register-Q");
 let initRegisterM = document.getElementById("init-register-M");
+let finalResultSection = document.getElementById("final-result-section");
+
+// Card containing the result
+let resultCard = document.getElementById("result-card");
+let resultSection = document.getElementById("result-section");
 
 let alerts = document.getElementById("alert-div"); // The section containing all alerts
 
@@ -29,6 +34,7 @@ const ALL_MODE = 0;
 const STEP_BY_STEP_MODE = 1;
 
 solutionTable.showTableMessage("-");
+addCardMessage("Please enter valid binary operands", resultCard);
 
 /**
  * Shows an alert in the page
@@ -58,9 +64,9 @@ function clearAlerts(){
  * calls the errorCallback function. The context of the callback functions would be the XMLHttpRequest object
  * used to make the request.
  */
-function getResult(successCallback, errorCallback){
-    let dividend = dividendInput.value.trim();
-    let divisor = divisorInput.value.trim();
+function getResult(successCallback, errorCallback, dividend, divisor){
+    dividend = dividend.trim();
+    divisor = divisor.trim();
     let xml = new XMLHttpRequest();
     xml.addEventListener("load", successCallback);
     xml.addEventListener("error", errorCallback)
@@ -75,6 +81,7 @@ function getResult(successCallback, errorCallback){
  */
 function updateResults(result){
     let mode = Number(modeSelect.value);
+    removeCardMessages();
     nextStepButton.style.display = (mode === ALL_MODE) ? "none": "block";
     result.quotient = result.quotient.join("");
     result.remainder = result.remainder.join("");
@@ -101,12 +108,43 @@ function updateResults(result){
         for(let i=0; i<solution.length; ++i){
             solutionTable.insertRow([String(i + 1), solution[i].A, solution[i].Q]);
         }
+        finalResultSection.style.display = "block";
     }else{
         solutionTable.insertRow([1, solution[0].A, solution[0].Q]);
+        finalResultSection.style.display = "none";
     }
     
-
+    resultSection.style.display = "block";
     latestResult = result;
+}
+
+/**
+ * Adds a message in a bootstrap card, immediately right after the head section if it exists; otherwise,
+ * prepends the message in the bootstrap card
+ * @param {string} message - The message to display
+ * @param {Element} card - The card to place the message in
+ * @return {Element} - The message element
+ */
+function addCardMessage(message, card){
+    let messageElem = document.createElement("p");
+    messageElem.textContent = message;
+    messageElem.classList.add("text-center", "mt-3", "card-message");
+
+    let cardHead = card.querySelector(".card-head");
+    card.insertBefore(messageElem, cardHead);
+
+    return messageElem;
+}
+
+/**
+ * Removes all messages in a given card
+ * @param {Element} card - The card to remove the messages for
+ */
+function removeCardMessages(card){
+    let messages = document.getElementsByClassName("card-message");
+    for(let i=0; i<messages.length; ++i){
+        messages[i].remove();
+    }
 }
 
 /**
@@ -114,21 +152,25 @@ function updateResults(result){
  * if no more steps are available
  */
 function nextStep(){
+    if(!latestResult){
+        return;
+    }
     let currentStepCount = solutionTable.tableElem.querySelectorAll("tbody>tr").length;
     let solution = latestResult.solution;
     if(currentStepCount < solution.length){
         solutionTable.insertRow([String(currentStepCount + 1), solution[currentStepCount].A, solution[currentStepCount].Q]);
         currentStepCount++;
     }
-    if(currentStepCount === latestResult.solution.length){
+    if(currentStepCount === solution.length){
         nextStepButton.style.display = "none"
+        finalResultSection.style.display = "block";
     }
 }
 
 nextStepButton.addEventListener("click", nextStep);
 
 /**
- * Removes the existing results in the page and sets the latestResult variable to null
+ * Removes the existing results in the page and sets the latestResult variable to null.
  */
 function clearResults(){
     quotient.value = "";
@@ -139,6 +181,8 @@ function clearResults(){
     initRegisterQ.textContent = "";
     initRegisterM.textContent = "";
     nextStepButton.style.display = "none";
+    finalResultSection.style.display = "block";
+    resultSection.style.display = "none";
 }
 
 // Execute upon submitting the input form for division
@@ -146,8 +190,10 @@ divisionForm.formElement.addEventListener("submit", function(event){
     event.preventDefault();
     clearAlerts();
     clearResults();
+    removeCardMessages();
+    addCardMessage("Computing...", resultCard);
     solutionTable.showTableMessage("-");
-    divisionForm.showLoading("");
+    divisionForm.setEnabled(false);
     let loadingAlert = addAlert("alert-secondary", "Computing...");
     let spinnerElem = spinner.createSpinnerBorder();
     spinnerElem.classList.add("spinner-border-sm");
@@ -156,6 +202,7 @@ divisionForm.formElement.addEventListener("submit", function(event){
     
     getResult(function(){
         clearAlerts();
+        removeCardMessages();
         let result;
         try{
             result = JSON.parse(this.responseText);
@@ -167,13 +214,15 @@ divisionForm.formElement.addEventListener("submit", function(event){
             updateResults(result);
         }else{
             let message = result.message || "Cannot communicate with server rigth now.";
+            addCardMessage("Invalid input", resultCard);
             addAlert("alert-danger", message);
         }
-        divisionForm.removeLoading();
+        divisionForm.setEnabled(true);
     }, function(){
         clearAlerts();
+        addCardMessage("Error.");
         addAlert("alert-danger", "Cannot communicate with server rigth now.");
-        divisionForm.removeLoading();
-    });
+        divisionForm.setEnabled(true);
+    }, dividendInput.value, divisorInput.value);
 });
 
